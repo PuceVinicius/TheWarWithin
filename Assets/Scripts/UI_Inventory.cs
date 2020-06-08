02f7953f8d975855c6ebd28f9f0c841680ab54d4
Vinícius Moreno <vinicius.moreno.ismart@gmail.com>
2020-06-08 20:53:42 -0300

    fixing .gitignore

---
+++ Assets/Scripts/UI_Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Inventory : MonoBehaviour
+{
+
+	private Inventory inventory;
+	private Transform item_slot_container;
+	private Transform item_slot_template;
+
+	private void Awake() {
+		item_slot_container = transform.Find("itemSlotContainer");
+		item_slot_template = item_slot_container.Find("itemSlotTemplate")
+	}
+
+	public void set_inventory(Inventory inventory) {
+		this.inventory = inventory;
+	}
+}
