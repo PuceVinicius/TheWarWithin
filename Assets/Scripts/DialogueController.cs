e19da8323eb1e3eb755c665a166bfd5f5a43f102
PuceVinicius <vinicius.moreno.ismart@gmail.com>
2020-10-13 04:27:22 -0300

    WIP prototype changes removed monster and collision boxes added new barriers added new interactions imported blood stamps and a new character upgraded some materials to HDRP

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour {
+    [SerializeField] private Text dialogueText;
+    [SerializeField] private List<string> dialogues;
+    [SerializeField] private List<float> timers;
+
+    private int index = -1;
+
+    private Coroutine coroutine;
+
+    public void ShowNextDialogue() {
+        coroutine = StartCoroutine(ChangeText());
+    }
+
+    public void HideDialogue() {
+        dialogueText.text = "";
+    }
+
+    private IEnumerator ChangeText() {
+        index++;
+        dialogueText.text = dialogues[index];
+
+        yield return new WaitForSeconds(timers[index]);
+
+        dialogueText.text = "";
+    }
+
+}
